9e102d19050c74324a482623fe1edc99f8404335
callendev20 <callendev20@gmail.com>
2024-08-19 01:23:25 +0900

    :tada: Init handong-ut-carpool

---
+++ src/main/java/com/handongapp/handongutcarpool/HandongUtCarpoolApplication.java
@@ -0,0 +1,13 @@
+package com.handongapp.handongutcarpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandongUtCarpoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandongUtCarpoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/handongapp/handongutcarpool/HandongUtCarpoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handongapp.handongutcarpool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandongUtCarpoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
