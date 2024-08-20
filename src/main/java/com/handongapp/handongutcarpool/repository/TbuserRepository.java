56be5edbb188047c886082edb7ab43e1e67c126b
callendev20 <callendev20@gmail.com>
2024-08-20 20:32:47 +0900

    ✅ Add method to TbuserRepository - findByHakbun, existsByHakbun

--- src/main/java/com/handongapp/handongutcarpool/repository/TbuserRepository.java
+++ src/main/java/com/handongapp/handongutcarpool/repository/TbuserRepository.java
@@ -4,7 +4,10 @@ import com.handongapp.handongutcarpool.domain.Tbuser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TbuserRepository extends JpaRepository<Tbuser, String> {
-
+    Optional<Tbuser> findByHakbun(String hakbun);
+    Boolean existsByHakbun(String hakbun);
 }
