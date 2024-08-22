c9b38b6a84e5ecb9cc9017f591e3caa757efe82b
callendev20 <callendev20@gmail.com>
2024-08-22 13:03:28 +0900

    🧑‍💻 Rename DuplicateCheckAspect to DataIntegrityViolationAspect

--- src/main/java/com/handongapp/handongutcarpool/aspects/DataIntegrityViolationAspect.java
+++ src/main/java/com/handongapp/handongutcarpool/aspects/DataIntegrityViolationAspect.java
@@ -1,6 +1,5 @@
 package com.handongapp.handongutcarpool.aspects;
 
-import com.handongapp.handongutcarpool.exception.DuplicateException;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -9,14 +8,14 @@ import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-public class DuplicateCheckAspect {
+public class DataIntegrityViolationAspect {
 
     @Around("execution(* com.handongapp.handongutcarpool.service.*.create(..))")
     public Object handleDuplicateEntry(ProceedingJoinPoint joinPoint) throws Throwable {
         try {
             return joinPoint.proceed();
         } catch (DataIntegrityViolationException e) {
-            throw new DuplicateException("중복된 데이터가 존재합니다.");
+            throw new DataIntegrityViolationException("데이터 무결성 위반 : " + e.getMessage());
         }
     }
 }
