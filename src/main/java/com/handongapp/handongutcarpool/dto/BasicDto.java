9c30a628b5e28b9a3d137710c50d21f997756061
callendev20 <callendev20@gmail.com>
2024-08-24 13:48:54 +0900

    :white_check_mark: Add BasicDto

---
+++ src/main/java/com/handongapp/handongutcarpool/dto/BasicDto.java
@@ -0,0 +1,17 @@
+package com.handongapp.handongutcarpool.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+public class BasicDto {
+    @Builder
+    @Schema
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class IdResDto{
+        @Schema(description = "id", example="UUID")
+        private String id;
+    }
+}
