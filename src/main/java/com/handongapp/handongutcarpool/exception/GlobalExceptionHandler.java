2f418938fba739c775a374c86d6f8ea31b4c0ba2
callendev20 <callendev20@gmail.com>
2024-08-24 15:10:07 +0900

    ✨Implement  GlobalExceptionHandler - RestControllerAdvice

---
+++ src/main/java/com/handongapp/handongutcarpool/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.handongapp.handongutcarpool.exception;
+
+import com.handongapp.handongutcarpool.dto.BasicDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoAuthorizationException.class)
+    public ResponseEntity<BasicDto.IdResDto> handleNoAuthorizationException(NoAuthorizationException ex) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body(BasicDto.IdResDto.builder().id(ex.getMessage()).build());
+    }
+
+    @ExceptionHandler(NoMatchingDataException.class)
+    public ResponseEntity<BasicDto.IdResDto> handleNoMatchingDataExceptionException(NoMatchingDataException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(BasicDto.IdResDto.builder().id(ex.getMessage()).build());
+    }
+
+}
