8ae522ae7c54f1d4001d6fba1011cab13727468f
callendev20 <callendev20@gmail.com>
2024-08-26 20:27:11 +0900

    :white_check_mark: Add GroupExceptionHandler

---
+++ src/main/java/com/handongapp/handongutcarpool/exception/GroupUserExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.handongapp.handongutcarpool.exception;
+
+import com.handongapp.handongutcarpool.controller.TbgroupTbuserRestController;
+import com.handongapp.handongutcarpool.dto.TbgroupTbuserDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(assignableTypes = {TbgroupTbuserRestController.class})
+public class GroupUserExceptionHandler {
+    @ExceptionHandler(UserAlreadyInGroupException.class)
+    public ResponseEntity<TbgroupTbuserDto.EnterGroupResDto> handleUserAlreadyInGroupException(UserAlreadyInGroupException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(TbgroupTbuserDto.EnterGroupResDto.builder().role(ex.getMessage()).build());
+    }
+
+    @ExceptionHandler(NoMatchingDataException.class)
+    public ResponseEntity<TbgroupTbuserDto.EnterGroupResDto> handleNoMatchingDataException(NoMatchingDataException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(TbgroupTbuserDto.EnterGroupResDto.builder().role(ex.getMessage()).build());
+    }
+
+}
