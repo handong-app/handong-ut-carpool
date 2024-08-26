fa5e62bff8a2dd8f888813daf1e0c3dcd79b672b
callendev20 <callendev20@gmail.com>
2024-08-27 00:48:16 +0900

    ✅ Add assignableTypes to GlobalExceptionHandler

--- src/main/java/com/handongapp/handongutcarpool/exception/GlobalExceptionHandler.java
+++ src/main/java/com/handongapp/handongutcarpool/exception/GlobalExceptionHandler.java
@@ -1,12 +1,14 @@
 package com.handongapp.handongutcarpool.exception;
 
+import com.handongapp.handongutcarpool.controller.TbgroupRestController;
+import com.handongapp.handongutcarpool.controller.TbuserRestController;
 import com.handongapp.handongutcarpool.dto.BasicDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@RestControllerAdvice(assignableTypes = {TbuserRestController.class, TbgroupRestController.class})
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoAuthorizationException.class)
