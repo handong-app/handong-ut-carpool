7f31c52c6023dcc071a5d785b03172c1ea40b27f
callendev20 <callendev20@gmail.com>
2024-08-29 19:17:46 +0900

    :white_check_mark: Implement userCount, isGroupFull - TbgroupTbuserService

--- src/main/java/com/handongapp/handongutcarpool/service/TbgroupTbuserService.java
+++ src/main/java/com/handongapp/handongutcarpool/service/TbgroupTbuserService.java
@@ -1,9 +1,11 @@
 package com.handongapp.handongutcarpool.service;
 
+import com.handongapp.handongutcarpool.dto.BasicDto;
 import com.handongapp.handongutcarpool.dto.TbgroupTbuserDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface TbgroupTbuserService {
     TbgroupTbuserDto.EnterGroupResDto enter(TbgroupTbuserDto.EnterGroupReqDto param);
+    TbgroupTbuserDto.UserCountResDto userCount(BasicDto.IdReqDto param);
 }
--- src/main/java/com/handongapp/handongutcarpool/service/impl/TbgroupTbuserServiceImpl.java
+++ src/main/java/com/handongapp/handongutcarpool/service/impl/TbgroupTbuserServiceImpl.java
@@ -1,9 +1,12 @@
 package com.handongapp.handongutcarpool.service.impl;
 
 
+import com.handongapp.handongutcarpool.dto.BasicDto;
 import com.handongapp.handongutcarpool.dto.TbgroupTbuserDto;
+import com.handongapp.handongutcarpool.exception.GroupFullException;
 import com.handongapp.handongutcarpool.exception.NoMatchingDataException;
 import com.handongapp.handongutcarpool.exception.UserAlreadyInGroupException;
+import com.handongapp.handongutcarpool.mapper.TbgroupTbuserMapper;
 import com.handongapp.handongutcarpool.repository.TbgroupRepository;
 import com.handongapp.handongutcarpool.repository.TbuserRepository;
 import com.handongapp.handongutcarpool.repository.TbuserTbgroupRepository;
@@ -20,11 +23,13 @@ public class TbgroupTbuserServiceImpl implements TbgroupTbuserService {
     private final TbuserRepository tbuserRepository;
     private final TbgroupRepository tbgroupRepository;
     private final TbuserTbgroupRepository tbuserTbgroupRepository;
+    private final TbgroupTbuserMapper tbgroupTbuserMapper;
 
-    public TbgroupTbuserServiceImpl(TbuserRepository tbuserRepository, TbgroupRepository tbgroupRepository, TbuserTbgroupRepository tbuserTbgroupRepository) {
+    public TbgroupTbuserServiceImpl(TbuserRepository tbuserRepository, TbgroupRepository tbgroupRepository, TbuserTbgroupRepository tbuserTbgroupRepository, TbgroupTbuserMapper tbgroupTbuserMapper) {
         this.tbuserRepository = tbuserRepository;
         this.tbgroupRepository = tbgroupRepository;
         this.tbuserTbgroupRepository = tbuserTbgroupRepository;
+        this.tbgroupTbuserMapper = tbgroupTbuserMapper;
     }
 
     @Override
@@ -37,6 +42,9 @@ public class TbgroupTbuserServiceImpl implements TbgroupTbuserService {
                     if (Boolean.TRUE.equals(isUserInGroup(TbgroupTbuserDto.IsUserInGroupServDto.builder().tbuserId(param.getTbuserId()).tbgroupId(param.getTbgroupId()).build()))) {
                         throw new UserAlreadyInGroupException("User Already Exists");
                     }
+                    if (Boolean.TRUE.equals(isGroupFull(BasicDto.IdReqDto.builder().id(param.getTbgroupId()).build()).getIsFull())){
+                        throw new GroupFullException("Group Full");
+                    }
                     else {
                         return tbuserTbgroupRepository.save(param.toEntity()).toEnterGroupResDto();
                     }
@@ -46,6 +54,16 @@ public class TbgroupTbuserServiceImpl implements TbgroupTbuserService {
 
     public Boolean isUserInGroup(TbgroupTbuserDto.IsUserInGroupServDto param){
         return tbuserTbgroupRepository.existsByTbgroupIdAndTbuserId(param.getTbgroupId(), param.getTbuserId());
+    }
+
+    public TbgroupTbuserDto.IsFullServDto isGroupFull(BasicDto.IdReqDto param){
+        return TbgroupTbuserDto.IsFullServDto.builder()
+                .isFull(tbgroupTbuserMapper.userCount(param).getTbuserCount() >= tbgroupRepository.findById(param.getId()).map(group -> group.getMaxCount()).orElseThrow(() -> new NoMatchingDataException("Group not found")))
+                .build();
+    }
+
+    public TbgroupTbuserDto.UserCountResDto userCount(BasicDto.IdReqDto param){
+        return tbgroupTbuserMapper.userCount(param);
     }
 
 }
