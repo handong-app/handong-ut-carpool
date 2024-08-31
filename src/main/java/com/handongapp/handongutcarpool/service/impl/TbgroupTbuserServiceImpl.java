00cfabb17be42e04b61bf8c33f876064a6ce1c62
callendev20 <callendev20@gmail.com>
2024-09-01 01:35:56 +0900

    :white_check_mark: Handle cases where the user has entered and left the room on enter - TbgroupTbuserService.enter

--- src/main/java/com/handongapp/handongutcarpool/service/impl/TbgroupTbuserServiceImpl.java
+++ src/main/java/com/handongapp/handongutcarpool/service/impl/TbgroupTbuserServiceImpl.java
@@ -2,17 +2,20 @@ package com.handongapp.handongutcarpool.service.impl;
 
 
 import com.handongapp.handongutcarpool.domain.Tbgroup;
+import com.handongapp.handongutcarpool.domain.TbgroupTbuser;
 import com.handongapp.handongutcarpool.dto.BasicDto;
 import com.handongapp.handongutcarpool.dto.TbgroupTbuserDto;
 import com.handongapp.handongutcarpool.exception.*;
 import com.handongapp.handongutcarpool.mapper.TbgroupTbuserMapper;
 import com.handongapp.handongutcarpool.repository.TbgroupRepository;
 import com.handongapp.handongutcarpool.repository.TbuserRepository;
-import com.handongapp.handongutcarpool.repository.TbuserTbgroupRepository;
+import com.handongapp.handongutcarpool.repository.TbgroupTbuserRepository;
 import com.handongapp.handongutcarpool.service.TbgroupTbuserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class TbgroupTbuserServiceImpl implements TbgroupTbuserService {
@@ -21,30 +24,34 @@ public class TbgroupTbuserServiceImpl implements TbgroupTbuserService {
 
     private final TbuserRepository tbuserRepository;
     private final TbgroupRepository tbgroupRepository;
-    private final TbuserTbgroupRepository tbuserTbgroupRepository;
+    private final TbgroupTbuserRepository tbgroupTbuserRepository;
     private final TbgroupTbuserMapper tbgroupTbuserMapper;
 
-    public TbgroupTbuserServiceImpl(TbuserRepository tbuserRepository, TbgroupRepository tbgroupRepository, TbuserTbgroupRepository tbuserTbgroupRepository, TbgroupTbuserMapper tbgroupTbuserMapper) {
+    public TbgroupTbuserServiceImpl(TbuserRepository tbuserRepository, TbgroupRepository tbgroupRepository, TbgroupTbuserRepository tbgroupTbuserRepository, TbgroupTbuserMapper tbgroupTbuserMapper) {
         this.tbuserRepository = tbuserRepository;
         this.tbgroupRepository = tbgroupRepository;
-        this.tbuserTbgroupRepository = tbuserTbgroupRepository;
+        this.tbgroupTbuserRepository = tbgroupTbuserRepository;
         this.tbgroupTbuserMapper = tbgroupTbuserMapper;
     }
 
     @Override
     public TbgroupTbuserDto.EnterGroupResDto enter(TbgroupTbuserDto.EnterGroupReqDto param){
         return tbgroupRepository.findById(param.getTbgroupId())
-                .map(existingTbgroup -> {
-                    isValid(param);
-                    return tbuserTbgroupRepository.save(param.toEntity()).toEnterGroupResDto();
-                })
+                .map(existingTbgroup -> tbgroupTbuserRepository.save(
+                        validatedAndReturn(param)
+                                .map(tbuserTbgroup -> {
+                                    tbuserTbgroup.setDeleted("N");
+                                    return tbuserTbgroup;
+                                })
+                                .orElse(param.toEntity())
+                ).toEnterGroupResDto())
                 .orElseThrow(() -> new NoMatchingDataException("Group not found"));
     }
 
     @Override
     public TbgroupTbuserDto.LeaveGroupResDto leave(TbgroupTbuserDto.LeaveGroupReqDto param){
-        tbuserTbgroupRepository.save(
-                tbuserTbgroupRepository.findByTbgroupIdAndTbuserId(param.getTbgroupId(), param.getTbuserId())
+        tbgroupTbuserRepository.save(
+                tbgroupTbuserRepository.findByTbgroupIdAndTbuserId(param.getTbgroupId(), param.getTbuserId())
                         .map(tbuserTbgroup -> {
                         if(tbuserTbgroup.getDeleted().equals("Y")) throw new UserAlreadyLeavedException("User is already leaved");
                         tbuserTbgroup.setDeleted("Y");
@@ -56,23 +63,23 @@ public class TbgroupTbuserServiceImpl implements TbgroupTbuserService {
     }
 
 
-    private void isValid(TbgroupTbuserDto.EnterGroupReqDto param){
+    private Optional<TbgroupTbuser> validatedAndReturn(TbgroupTbuserDto.EnterGroupReqDto param){
         if (!tbuserRepository.existsById(param.getTbuserId())) {
             throw new NoMatchingDataException("User not found");
         }
-        if (Boolean.TRUE.equals(isUserInGroup(TbgroupTbuserDto.IsUserInGroupServDto.builder().tbuserId(param.getTbuserId()).tbgroupId(param.getTbgroupId()).build()))) {
-            throw new UserAlreadyInGroupException("User Already Exists");
-        }
-        if (Boolean.TRUE.equals(isGroupFull(BasicDto.IdReqDto.builder().id(param.getTbgroupId()).build()).getIsFull())){
-            throw new GroupFullException("Group Full");
-        }
-        if (Boolean.TRUE.equals(isGroupLock(BasicDto.IdReqDto.builder().id(param.getTbgroupId()).build()).getIsLock())){
-            throw new GroupLockedException("Group Locked");
-        }
-    }
-
-    public Boolean isUserInGroup(TbgroupTbuserDto.IsUserInGroupServDto param){
-        return tbuserTbgroupRepository.existsByTbgroupIdAndTbuserId(param.getTbgroupId(), param.getTbuserId());
+        return tbgroupTbuserRepository.findByTbgroupIdAndTbuserId(param.getTbgroupId(), param.getTbuserId())
+                .map(
+                    tbgroupTbuser -> {
+                        if(tbgroupTbuser.getDeleted().equals("N")) throw new UserAlreadyInGroupException("User is already in group");
+                        if (Boolean.TRUE.equals(isGroupFull(BasicDto.IdReqDto.builder().id(param.getTbgroupId()).build()).getIsFull())){
+                            throw new GroupFullException("Group Full");
+                        }
+                        if (Boolean.TRUE.equals(isGroupLock(BasicDto.IdReqDto.builder().id(param.getTbgroupId()).build()).getIsLock())){
+                            throw new GroupLockedException("Group Locked");
+                        }
+                        return tbgroupTbuser;
+                    }
+                );
     }
 
     public TbgroupTbuserDto.IsFullServDto isGroupFull(BasicDto.IdReqDto param){
