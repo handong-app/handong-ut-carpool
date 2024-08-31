10c65643cf56f4cde24f133908f62077c7b24542
callendev20 <callendev20@gmail.com>
2024-09-01 00:46:00 +0900

    :white_check_mark: Add findByTbgroupIdAndTbuserId - TbuserTbgroupRepository

--- src/main/java/com/handongapp/handongutcarpool/repository/TbuserTbgroupRepository.java
+++ src/main/java/com/handongapp/handongutcarpool/repository/TbuserTbgroupRepository.java
@@ -4,7 +4,10 @@ import com.handongapp.handongutcarpool.domain.TbgroupTbuser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TbuserTbgroupRepository extends JpaRepository<TbgroupTbuser, String> {
     Boolean existsByTbgroupIdAndTbuserId(String tbgroupId, String tbuserId);
+    Optional<TbgroupTbuser> findByTbgroupIdAndTbuserId(String tbgroupId, String tbuserId);
 }
