ac8482d2f2346a677e7ef0f70f478956e81f46f4
callendev20 <callendev20@gmail.com>
2024-09-27 15:38:04 +0900

    🐛 Remove assignableTypes to apply RestControllerAdvice global

--- src/main/java/com/handongapp/handongutcarpool/exception/GlobalExceptionHandler.java
+++ src/main/java/com/handongapp/handongutcarpool/exception/GlobalExceptionHandler.java
@@ -1,14 +1,13 @@
 package com.handongapp.handongutcarpool.exception;
 
-import com.handongapp.handongutcarpool.controller.TbgroupRestController;
-import com.handongapp.handongutcarpool.controller.TbuserRestController;
 import com.handongapp.handongutcarpool.dto.CommonDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice(assignableTypes = {TbuserRestController.class, TbgroupRestController.class})
+//@RestControllerAdvice(assignableTypes = {TbuserRestController.class, TbgroupRestController.class})
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoAuthorizationException.class)
