c18fdb7602e60e24fb9bf717f2929f7beea16df0
callendev20 <callendev20@gmail.com>
2024-09-27 15:56:40 +0900

    :white_check_mark: Add getLuggageCount to TbgroupTbuserMapper

--- src/main/java/com/handongapp/handongutcarpool/mapper/TbgroupTbuserMapper.java
+++ src/main/java/com/handongapp/handongutcarpool/mapper/TbgroupTbuserMapper.java
@@ -1,8 +1,10 @@
 package com.handongapp.handongutcarpool.mapper;
 
+import com.handongapp.handongutcarpool.domain.TbgroupTbuser;
 import com.handongapp.handongutcarpool.dto.CommonDto;
 import com.handongapp.handongutcarpool.dto.TbgroupTbuserDto;
 
 public interface TbgroupTbuserMapper {
     TbgroupTbuserDto.UserCountResDto getUserCount(CommonDto.IdReqDto param);
+    TbgroupTbuserDto.LuggageCountResDto getLuggageCount(CommonDto.IdReqDto param);
 }
