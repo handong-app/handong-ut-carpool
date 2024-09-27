632970acf661b696eaa38b8727e2e5db4a3fcaaa
callendev20 <callendev20@gmail.com>
2024-09-27 15:56:10 +0900

    :white_check_mark: Add getMaxLuggageCount, getIsLock to TbgroupMapper

--- src/main/java/com/handongapp/handongutcarpool/mapper/TbgroupMapper.java
+++ src/main/java/com/handongapp/handongutcarpool/mapper/TbgroupMapper.java
@@ -1,10 +1,11 @@
 package com.handongapp.handongutcarpool.mapper;
 
 import com.handongapp.handongutcarpool.dto.CommonDto;
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Update;
 
 public interface TbgroupMapper {
     @Update("UPDATE tbgroup SET deleted = 'Y' WHERE id = #{id}")
     int groupAdminLeave(CommonDto.IdReqDto param);
+    int getMaxLuggageCount(CommonDto.IdReqDto param);
+    Boolean getIsLock(CommonDto.IdReqDto param);
 }
