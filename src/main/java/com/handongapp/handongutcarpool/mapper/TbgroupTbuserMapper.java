bdbdf170994c7fee89004d602d030daaeab6c3cc
callendev20 <callendev20@gmail.com>
2024-09-29 21:13:21 +0900

    ✅ Implement TbgroupTbuserMapper.getDetailFromGroupUser, isUserInGroup

--- src/main/java/com/handongapp/handongutcarpool/mapper/TbgroupTbuserMapper.java
+++ src/main/java/com/handongapp/handongutcarpool/mapper/TbgroupTbuserMapper.java
@@ -6,4 +6,6 @@ import com.handongapp.handongutcarpool.dto.TbgroupTbuserDto;
 public interface TbgroupTbuserMapper {
     TbgroupTbuserDto.PassengerCountResDto getPassengerCount(CommonDto.IdReqDto param);
     TbgroupTbuserDto.LuggageCountResDto getLuggageCount(CommonDto.IdReqDto param);
+    TbgroupTbuserDto.DetailFromGroupUserServDto getDetailFromGroupUser(CommonDto.IdReqDto param);
+    TbgroupTbuserDto.IsUserInGroupServDto isUserInGroup(CommonDto.IdReqDto param);
 }
