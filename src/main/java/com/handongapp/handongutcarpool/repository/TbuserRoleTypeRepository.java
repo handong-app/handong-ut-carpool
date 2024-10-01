a1b0bdc481ff25711ddaf0aea0b710dc6a565d8c
callendev20 <callendev20@gmail.com>
2024-10-01 10:40:57 +0900

    :white_check_mark: Add TbuserRoleType entity, repo

---
+++ src/main/java/com/handongapp/handongutcarpool/repository/TbuserRoleTypeRepository.java
@@ -0,0 +1,7 @@
+package com.handongapp.handongutcarpool.repository;
+
+import com.handongapp.handongutcarpool.domain.TbuserRoleType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TbuserRoleTypeRepository extends JpaRepository<TbuserRoleType, String> {
+}
