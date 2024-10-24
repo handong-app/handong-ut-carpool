18961e037a83f1eabf2ff51aeb5887c832b1d98e
callendev20 <callendev20@gmail.com>
2024-10-24 20:05:13 +0900

    :white_check_mark: Add repository methods related with Roles

--- src/main/java/com/handongapp/handongutcarpool/repository/TbuserRepository.java
+++ src/main/java/com/handongapp/handongutcarpool/repository/TbuserRepository.java
@@ -1,6 +1,7 @@
 package com.handongapp.handongutcarpool.repository;
 
 import com.handongapp.handongutcarpool.domain.Tbuser;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,7 @@ import java.util.Optional;
 @Repository
 public interface TbuserRepository extends JpaRepository<Tbuser, String> {
     Optional<Tbuser> findByHakbun(String hakbun);
-    Boolean existsByHakbun(String hakbun);
+
+    @EntityGraph(attributePaths = {"tbuserRoleType.roleType"})
+    Optional<Tbuser> findEntityGraphRoleTypeById(String id);
 }
